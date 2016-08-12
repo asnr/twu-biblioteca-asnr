f4a65e4f402d52e36714ae7fa9599210872b3d6c
asnr <asarnari@thoughtworks.com>
2016-08-12 12:33:28 +1000

    Finished moving logic out of AppController into AppState objects.
    AppController is now just a skeleton that delegates everything to the AppState objects.

--- src/com/twu/biblioteca/controller/AppController.java
+++ src/com/twu/biblioteca/controller/AppController.java
@@ -9,10 +9,10 @@ public class AppController {
 
     public enum State {
         Login,
-        MainMenu, MainMenuInvalidOption,
-        ListBooks, SuccessfulBookCheckout, UnsuccessfulBookCheckout,
-        ReturnBooks, SuccessfulReturn, UnsuccessfulReturn,
-        ListMovies, SuccessfulMovieCheckout,
+        MainMenu,
+        ListBooks,
+        ReturnBooks,
+        ListMovies,
         Finished
     }
 
@@ -32,35 +32,34 @@ public class AppController {
         this.books = books;
         this.movies = movies;
 
-
+        switch (state) {
+            case Login:
+                appState = new LoginState(books, movies);
+                break;
+            case MainMenu:
+                appState = new MainMenuState(books, movies);
+                break;
+            case ListBooks:
+                appState = new BorrowBookState(books, movies);
+                break;
+            case ReturnBooks:
+                appState = new ReturnBookState(books, movies);
+                break;
+            case ListMovies:
+                appState = new BorrowMovieState(books, movies);
+                break;
+            case Finished:
+                appState = new QuitState(books, movies);
+                break;
+            default:
+                appState = null;
+        }
     }
 
 
     public Screen getCurrentScreen() {
-        switch (state) {
-            case Login:
-                // This is only here because login is the default start screen
-                if (appState == null) {
-                    appState = new LoginState(books, movies);
-                }
-                return appState.getScreen();
-            case MainMenu:
-                return new MainMenuScreen();
-            case MainMenuInvalidOption:
-                return new InvalidOptionScreen();
-            case ListBooks:
-                return appState.getScreen();
-            case ReturnBooks:
-                return appState.getScreen();
-            case ListMovies:
-                return appState.getScreen();
-            case Finished:
-                return appState.getScreen();
-            default:
-                return null;
-        }
+        return appState.getScreen();
     }
-
 
     public Screen getNextScreen(String input) {
 
@@ -71,113 +70,7 @@ public class AppController {
 
 
     private void updateState(String input) {
-
-        switch (state) {
-            case Login:
-                if (appState == null) {
-                    appState = new LoginState(books, movies);
-                }
-
-                appState = appState.nextState(input);
-                if (appState instanceof MainMenuState) {
-                    state = State.MainMenu;
-                }
-                break;
-
-            case MainMenu:
-
-                state = updateMainMenuState(input);
-
-                if (state == ListBooks) {
-                    appState = new BorrowBookState(books, movies);
-                } else if (state == ReturnBooks) {
-                    appState = new ReturnBookState(books, movies);
-                } else if (state == ListMovies) {
-                    appState = new BorrowMovieState(books, movies);
-                } else if (state == Finished) {
-                    appState = new QuitState(books, movies);
-                }
-
-                break;
-
-            case MainMenuInvalidOption:
-
-                state = State.MainMenu;
-                break;
-
-            case ListBooks:
-                if (appState == null) {
-                    appState = new BorrowBookState(books, movies);
-                }
-
-                appState = appState.nextState(input);
-                if (appState instanceof MainMenuState) {
-                    appState = null;
-                    state = State.MainMenu;
-                }
-                break;
-
-            case ReturnBooks:
-                if (appState == null) {
-                    appState = new ReturnBookState(books, movies);
-                }
-
-                appState = appState.nextState(input);
-                if (appState instanceof MainMenuState) {
-                    appState = null;
-                    state = State.MainMenu;
-                }
-                break;
-
-            case ListMovies:
-                if (appState == null) {
-                    appState = new BorrowMovieState(books, movies);
-                }
-
-                appState = appState.nextState(input);
-                if (appState instanceof MainMenuState) {
-                    appState = null;
-                    state = State.MainMenu;
-                }
-                break;
-
-            case Finished:
-
-                if (appState == null) {
-                    appState = new QuitState(books, movies);
-                }
-
-                appState = appState.nextState(input);
-                break;
-
-            default:
-
-                state = State.MainMenu;
-                break;
-        }
+        appState = appState.nextState(input);
     }
 
-
-    private State updateMainMenuState(String input) {
-        if (input.equals(MainMenuScreen.ListBooksOption)) {
-
-            return State.ListBooks;
-
-        } else if (input.equals(MainMenuScreen.ReturnBooksOption)) {
-
-            return State.ReturnBooks;
-
-        } else if (input.equals(MainMenuScreen.ListMoviesOption)) {
-
-            return State.ListMovies;
-
-        } else if (input.equals(MainMenuScreen.QuitOption)) {
-
-            return State.Finished;
-
-        } else {
-
-            return State.MainMenuInvalidOption;
-        }
-    }
 }
